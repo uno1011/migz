0b233421232f3de1d8fa2290f9c5323ed0e18155
uno1011 <151750618+uno1011@users.noreply.github.com>
2023-12-25 21:04:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-document.getElementById('greetButton').addEventListener('click', function() {
-  const greetings = [
-    'Hello!',
-    'Hi there!',
-    'Greetings!',
-    'Welcome!',
-    'Hey!'
-  ];
 
-  // Randomly select a greeting from the array
-  const randomIndex = Math.floor(Math.random() * greetings.length);
-  const selectedGreeting = greetings[randomIndex];
-
-  // Display the greeting message
-  document.getElementById('greetingText').textContent = selectedGreeting;
-  document.getElementById('greetingMessage').style.display = 'block';
-});
